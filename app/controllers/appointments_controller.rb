e758d50a23ecec40cf66e46ba10d882030bc2c29
AsafDavidov <adavidov@conncoll.edu>
2019-04-04 14:59:30 -0400

    post nested appointment working and removed index/show for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,17 +1,19 @@
 class AppointmentsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
 
-  def index
-    @appointments = Appointment.all
-    render json: @appointments, status: :ok
-  end
-  def show
+  def create
+    # first i find the nested schedule given the ID
     @schedule = Schedule.find_by_id(params[:schedule_id])
     if @schedule
-      @appointment = Appointment.find_by_id(params[:id])
-      if @appointment
-        render json: @appointment,status: :ok
+      #Here is a quick check to make sure that the validations have passed
+      @appointment = Appointment.new(appointment_params)
+      @appointment[:schedule_id] = @schedule.id
+      if @appointment.valid?
+        #if the validations pass save the appointment
+        @appointment.save
+        render json: @appointment, status: :ok
       else
-        render json: {errorMessage:"no appointment with id: #{params[:id]}"}, status: :not_found
+        render json: {errorMessage: "#{@appointment.errors.full_messages}"}
       end
     else
       render json: {errorMessage:"no schedule with id: #{params[:schedule_id]}"}, status: :not_found
@@ -19,17 +21,8 @@ class AppointmentsController < ApplicationController
 
   end
 
-  def create
-    @appointment = Appointment.create(appointment_params)
-    if @appointment.valid?
-      render json: @appointment, status: :ok
-    else
-      render json: {errorMessage: "#{@appointment.errors.full_messages}"}
-    end
-  end
-
   private
   def appointment_params
-    params.require(:appointment).permit(:name,:end_time,:start_time,:schedule_id)
+    params.require(:appointment).permit(:name,:end_time,:start_time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #Creating the routes specified for Schedules which is create, show, and destroy
-  resources :schedules, only: [:create, :show ,:index,:destroy] do 
+  resources :schedules, only: [:create, :show ,:index,:destroy] do
     #For appointments I placed the create route as well as the show and index routes so that they can be viewed.
-    resources :appointments, only: [:index,:show,:create]
+    resources :appointments, only: [:create]
   end
 end
