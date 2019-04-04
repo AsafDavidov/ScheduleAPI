9a20be6ba6c6a61f8a2012bd9438705c049e7427
AsafDavidov <adavidov@conncoll.edu>
2019-04-04 15:42:12 -0400

    complete API

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,28 @@
 class AppointmentsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
+  #+++++++++++++++++++++++++
+  #Temporarily remove the authenticity token that is within rails in order to test cURL routes
+  skip_before_action :verify_authenticity_token, only: [:create,:destroy]
+  #+++++++++++++++++++++++++
+
+  #commented out show and index for appointments for full restful interactions
+  #remmber to add routes
+  # def index
+  #   @appointments = Appointment.all
+  #   render json: @appointments, status: :ok
+  # end
+  # def show
+  #   @schedule = Schedule.find_by_id(params[:schedule_id])
+  #   if @schedule
+  #     @appointment = Appointment.find_by_id(params[:id])
+  #     if @appointment
+  #       render json: @appointment,status: :ok
+  #     else
+  #       render json: {errorMessage:"no appointment with id: #{params[:id]}"}, status: :not_found
+  #     end
+  #   else
+  #     render json: {errorMessage:"no schedule with id: #{params[:schedule_id]}"}, status: :not_found
+  #   end
+  # end
 
   def create
     # first i find the nested schedule given the ID
@@ -18,7 +41,26 @@ class AppointmentsController < ApplicationController
     else
       render json: {errorMessage:"no schedule with id: #{params[:schedule_id]}"}, status: :not_found
     end
+  end
 
+
+  #Optional destroy appointments since it was mentioned in the Supported requests but not
+  #in the appointment model description
+  def destroy
+    @schedule = Schedule.find_by_id(params[:schedule_id])
+    if @schedule
+      #Here is a quick check to make sure that the validations have passed
+      @appointment = Appointment.find_by_id(params[:id])
+      if @appointment
+        #if the validations pass save the appointment
+        @appointment.destroy
+        render json: @schedule, status: :ok
+      else
+        render json: {errorMessage: "no appointment with id: #{@appointment.errors.full_messages}"}
+      end
+    else
+      render json: {errorMessage:"no schedule with id: #{params[:schedule_id]}"}, status: :not_found
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   #Creating the routes specified for Schedules which is create, show, and destroy
   resources :schedules, only: [:create, :show ,:index,:destroy] do
     #For appointments I placed the create route as well as the show and index routes so that they can be viewed.
-    resources :appointments, only: [:create]
+    resources :appointments, only: [:create, :destroy]
   end
 end
